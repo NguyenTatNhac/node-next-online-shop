69fd070ef14afcdc07fe188cb8348b9922073307
Nhac Tat Nguyen <nhactat.nguyen_ext@zooplus.com>
2024-05-24 21:34:30 +0200

    Products route

--- app.ts
+++ app.ts
@@ -4,8 +4,9 @@ import cookieParser from 'cookie-parser';
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
+import productsRouter from './routes/products';
 
-var app = express();
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,9 +15,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
 // catch 404 error
-app.use((req, res, next) => {
+app.use((_req, res) => {
   res.sendStatus(404);
 });
 
