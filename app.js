8a1abff07a1b2d4a94af6a0a3adb5d9df7ed2a9d
Nhac Tat Nguyen <nhactat.nguyen_ext@zooplus.com>
2024-05-24 16:55:37 +0200

    Init express app + clean up

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 error
+app.use(function(req, res, next) {
+  res.sendStatus(404);
+});
+
+module.exports = app;
