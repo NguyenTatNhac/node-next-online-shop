eb6ffc8c1ecc01a5e40cc134ab45cd6e0e3dc905
Nhac Tat Nguyen <nhactat.nguyen_ext@zooplus.com>
2024-05-27 18:19:29 +0200

    Fix naming

--- src/repositories/ProductRepository.ts
+++ src/repositories/ProductRepository.ts
@@ -3,11 +3,11 @@ import sequelize from '../sequelize';
 import { ProductModel } from '../sequelize/models/Product.model';
 import { ModelStatic } from 'sequelize';
 
-const ProductModel = sequelize.model('Product') as ModelStatic<ProductModel>;
+const Model = sequelize.model('Product') as ModelStatic<ProductModel>;
 
 class ProductRepository {
   static async create(product: Product): Promise<Product> {
-    return await ProductModel.create(product);
+    return await Model.create(product);
   }
 }
 
